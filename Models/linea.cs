2ec94915058f210d02b46a8f98bb5e5508443cf9
josag <josag@GROOT>
2024-02-20 15:27:43 -0600

    Agregar archivos de proyecto.

---
+++ Models/linea.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace tabla1._1.Models
+{
+    [Table("Linea")]
+    public class linea
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string ubicacion { get; set; }
+        public string ajuste { get; set; }
+
+    }
+}
---
+++ Models/piezas.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace tabla1._1.Models
+{
+    [Table("Pieza")]
+    public class piezas
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Nparte { get; set; }
+
+        public string color { get; set; }
+        public string modelo { get; set; }
+
+
+    }
+}
